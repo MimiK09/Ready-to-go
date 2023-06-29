2fd87b0741bd9edb639518e5e5fa2766128c7eb5
Bouclet Emeline <mimi@MacBook-Air-de-Bouclet.local>
2023-06-29 22:29:11 +0200

    gestion bouton on/off

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,54 @@
 import "./App.css";
+import Curseur from "./assets/Components/Curseur";
 import { useState } from "react";
 
 function App() {
-	const [switch1, setSwitch1] = useState(false);
-	const [switch2, setSwitch2] = useState(false);
-	const [switch3, setSwitch3] = useState(false);
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
 
-	const final = () => {
-		if (switch1 && switch2 && switch3) {
-			return "finalok";
-		} else {
-			return "finalno";
-		}
-	};
+  const final = () => {
+    if (switch1 && switch2 && switch3) {
+      return "finalok";
+    } else {
+      return "finalno";
+    }
+  };
 
-	return (
-		<>
-			<header>
-				<div className="container">
-					<img
-						src="./src/assets/space-shuttle.png"
-						alt="fusée"
-						className="logo"
-					/>
-					<h1>Ready To Go</h1>
-				</div>
-			</header>
-			<main>
-				<div className="container">
-					<div className="curseurs">
-						<div className="curseur">
-							<p
-								onClick={() => {
-									setSwitch1(!switch1);
-								}}
-								className={switch1 ? "clicked" : "noclicked"}
-							>
-								ON
-							</p>
-							<p
-								onClick={() => {
-									setSwitch1(!switch1);
-								}}
-								className={switch1 ? "noclicked" : "clicked"}
-							>
-								OFF
-							</p>
-						</div>
-						<div className="curseur">
-							<p
-								onClick={() => {
-									setSwitch2(!switch2);
-								}}
-								className={switch2 ? "clicked" : "noclicked"}
-							>
-								ON
-							</p>
-							<p
-								onClick={() => {
-									setSwitch2(!switch2);
-								}}
-								className={switch2 ? "noclicked" : "clicked"}
-							>
-								OFF
-							</p>
-						</div>
-						<div className="curseur">
-							<p
-								onClick={() => {
-									setSwitch3(!switch3);
-								}}
-								className={switch3 ? "clicked" : "noclicked"}
-							>
-								ON
-							</p>
-							<p
-								onClick={() => {
-									setSwitch3(!switch3);
-								}}
-								className={switch3 ? "noclicked" : "clicked"}
-							>
-								OFF
-							</p>
-						</div>
-					</div>
-					<p className={final()}>No Way</p>
-				</div>
 
-				<div>
-					<p
-						onClick={() => {
-							setSwitch1(false);
-							setSwitch2(false);
-							setSwitch3(false);
-						}}
-						className="reset"
-					>
-						Reset
-					</p>
-				</div>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <header>
+        <div className="container">
+          <img src="./src/assets/space-shuttle.png" alt="fusée" className="logo" />
+          <h1>Ready To Go</h1>
+        </div>
+      </header>
+      <main>
+        <div className="container">
+          <div className="curseurs">
+            <Curseur switchState={switch1} setSwitchState={setSwitch1} />
+            <Curseur switchState={switch2} setSwitchState={setSwitch2} />
+            <Curseur switchState={switch3} setSwitchState={setSwitch3} />
+          </div>
+          <p className={final()}>No Way</p>
+        </div>
+
+        <div>
+          <p
+            onClick={() => {
+              setSwitch1(false);
+              setSwitch2(false);
+              setSwitch3(false);
+            }}
+            className="reset"
+          >
+            Reset
+          </p>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/assets/Components/Curseur.jsx
+++ src/assets/Components/Curseur.jsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
-
 const Curseur = (props) => {
-	const [isOn, setIsOn] = useState(false);
-	return (
-		<div className="curseur">
-			<p
-				onClick={() => {
-					setIsOn(!isOn);
-				}}
-				className={isOn ? "clicked" : "noclicked"}
-			>
-				ON
-			</p>
-			<p onClick={() => {
-					setIsOn(!isOn);
-				}} className={isOn ? "noclicked" : "clicked"}>OFF</p>
-		</div>
-	);
+
+
+  return (
+    <div className="curseur">
+      <p
+        onClick={() => {
+            props.setSwitchState(true);
+          }}
+        className={props.switchState ? "clicked" : "noclicked"}
+      >
+        ON
+      </p>
+      <p
+        onClick={() => {
+            props.setSwitchState(false);
+          }}
+        className={props.switchState ? "noclicked" : "clicked"}
+      >
+        OFF
+      </p>
+    </div>
+  );
 };
 
 export default Curseur;
